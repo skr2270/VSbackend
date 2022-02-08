c3312d873f99a537fce34e423a1d2ea44517384f
Sai Kumar Rayavarapu <sk.r.2270@gmail.com>
2022-02-08 16:13:35 +0530

    First Commit

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,4 @@
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, UploadedFiles, Put, Req, Res } from "@nestjs/common";
+import { User } from "../model/user.schema";
+import { UserService } from "../model/user.service";
+import { JwtService } from '@nestjs/jwt'
